45a0234c607e2b306f1ccf400f4f71d829970fd0
lookup69 <lookup69@gmail.com>
2014-05-22 16:58:19 +0800

    1) added rwlock function 2) added error return

--- CThread.cpp
+++ CThread.cpp
@@ -9,20 +9,30 @@
 
 #include "CThread.h"
 
-void CThread::mutexInit(pthread_mutex_t &mtx, mutextAttr_e mutexAttr)
+int CThread::mutexInit(pthread_mutex_t &mtx, mutextAttr_e mutexAttr)
 {
     pthread_mutexattr_t mtxAttr;
+    int ret;
 
-    pthread_mutexattr_init(&mtxAttr);
-    if (mutexAttr == MUTEX_ERROR_CHECK_E) 
-        pthread_mutexattr_settype(&mtxAttr, PTHREAD_MUTEX_ERRORCHECK);
-    if (mutexAttr == MUTEX_RECURSIVE_E) 
-        pthread_mutexattr_settype(&mtxAttr, PTHREAD_MUTEX_RECURSIVE);
-    else 
-        pthread_mutexattr_settype(&mtxAttr, PTHREAD_MUTEX_NORMAL);
+    if((ret = pthread_mutexattr_init(&mtxAttr)) < 0)
+        return ret;
+
+    if (mutexAttr == CTHREAD_MUTEX_ERROR_CHECK_E) {
+        if((ret = pthread_mutexattr_settype(&mtxAttr, PTHREAD_MUTEX_ERRORCHECK)) < 0)
+            goto _err;
+    } else if (mutexAttr == CTHREAD_MUTEX_RECURSIVE_E) {
+        if((ret = pthread_mutexattr_settype(&mtxAttr, PTHREAD_MUTEX_RECURSIVE)) < 0)
+            goto _err;
+    } else {
+        if((ret = pthread_mutexattr_settype(&mtxAttr, PTHREAD_MUTEX_NORMAL)) < 0)
+            goto _err;
+    }
 
     pthread_mutex_init(&mtx, &mtxAttr);
     pthread_mutexattr_destroy(&mtxAttr);
+
+_err:
+    return ret;
 }
 
 
--- CThread.h
+++ CThread.h
@@ -7,19 +7,19 @@
  *
  */
 
-#ifndef _myThread_CTHREAD_H_
-#define _myThread_CTHREAD_H_
+#ifndef _CTHREAD_H_
+#define _CTHREAD_H_
 #include <pthread.h>
 
 enum threadType_e {
-    JOIN_E,
-    DETACH_E
+    CTHREAD_JOIN_E,
+    CTHREAD_DETACH_E
 };
 
 enum mutextAttr_e {
-    MUTEX_NORMAL_E,
-    MUTEX_ERROR_CHECK_E,
-    MUTEX_RECURSIVE_E
+    CTHREAD_MUTEX_NORMAL_E,
+    CTHREAD_MUTEX_ERROR_CHECK_E,
+    CTHREAD_MUTEX_RECURSIVE_E
 };
 
 class CThread
@@ -29,54 +29,140 @@ private:
     threadType_e    m_threadType;
 
 private:
-    static void _cleanupHandler(void *obj)
-    {
-        CThread *_obj;
-        _obj = reinterpret_cast<CThread *>(obj);
-        _obj->cleanuUp();
-    }
-
     static void* _threadFunc(void *obj)
     {
         CThread *_obj;
         _obj = reinterpret_cast<CThread *>(obj);
 
-        pthread_cleanup_push(CThread::_cleanupHandler, obj);
         _obj->onStartup();
         _obj->process();
         _obj->onExit();
-        pthread_cleanup_pop(1);
 
         return NULL;
     }
 
 public:
-    CThread(threadType_e threadType = DETACH_E) {};
+    CThread(threadType_e threadType = CTHREAD_DETACH_E) :m_threadType(threadType) {};
 
     virtual ~CThread()
     {
-        pthread_cancel(getThreadId());
+        pthread_cancel(getThreadID());
     }
 
+    /*
+     *  mutex operation
+     */
+    static int mutexInit(pthread_mutex_t& mtx, mutextAttr_e mutexAttr = CTHREAD_MUTEX_NORMAL_E);
+
+    static int mutexDestroy(pthread_mutex_t& mtx)
+    {
+        return pthread_mutex_destroy(&mtx);
+    }
+
+    static int lock(pthread_mutex_t& mtx)
+    {
+        //printf("pthread_mutex_lock(%p)\n", &mtx);
+        return pthread_mutex_lock(&mtx);
+    }
+
+    static int tryLock(pthread_mutex_t& mtx)
+    {
+        return pthread_mutex_trylock(&mtx);
+    }
+
+    static int unlock(pthread_mutex_t& mtx)
+    {
+        //printf("pthread_mutex_unlock(%p)\n", &mtx);
+        return pthread_mutex_unlock(&mtx);
+    }
+    /*
+     *  read write lock
+     */
+    static int rwLockInit(pthread_rwlock_t& rwlock)
+    {
+        return pthread_rwlock_init(&rwlock, NULL);
+    }
+
+    static int rwLockDestroy(pthread_rwlock_t& rwlock)
+    {
+        return pthread_rwlock_destroy(&rwlock);
+    }
+
+    static int readLock(pthread_rwlock_t& rwlock)
+    {
+        return pthread_rwlock_rdlock(&rwlock);
+    }
+
+    static int readTryLock(pthread_rwlock_t& rwlock)
+    {
+        return pthread_rwlock_tryrdlock(&rwlock);
+    }
+
+    static int writeLock(pthread_rwlock_t& rwlock)
+    {
+        return pthread_rwlock_wrlock(&rwlock);
+    }
+
+    static int writeTryLock(pthread_rwlock_t& rwlock)
+    {
+        return pthread_rwlock_trywrlock(&rwlock);
+    }
+
+    static int readWriteUnlock(pthread_rwlock_t& rwlock)
+    {
+        return pthread_rwlock_unlock(&rwlock);
+    }
+
+    /*
+     *  condition operation
+     */
+    static int condInit(pthread_cond_t& cond)
+    {
+        return pthread_cond_init(&cond, NULL);
+    }
+
+    static int condDestroy(pthread_cond_t& cond)
+    {
+        return pthread_cond_destroy(&cond);
+    }
+
+    static int condWait(pthread_cond_t& cond, pthread_mutex_t& mtx)
+    {
+        return pthread_cond_wait(&cond, &mtx);
+    }
+
+    static int condTimedWait(pthread_cond_t& cond, pthread_mutex_t& mtx, const struct timespec& abstime)
+    {
+        return pthread_cond_timedwait(&cond, &mtx, &abstime);
+    }
+
+    static int condSignal(pthread_cond_t& cond)
+    {
+        return pthread_cond_signal(&cond);
+    }
+
+    static int condBroadcast(pthread_cond_t& cond)
+    {
+        return pthread_cond_broadcast(&cond);
+    }
+
+    /*
+     *  create thread
+     */
     int start(void)
     {
         int rtn;
 
         rtn = pthread_create(&m_tid, NULL, CThread::_threadFunc, this);
-        if (!rtn && (m_threadType == DETACH_E)) pthread_detach(m_tid);
+        if (!rtn && (m_threadType == CTHREAD_DETACH_E)) pthread_detach(m_tid);
 
         return rtn;
     }
 
-    /* 
-     *  thread body 
+    /*
+     *
      */
-    virtual void process(void) = 0;
-    virtual void onStartup(void) {}
-    virtual void onExit(void) {}
-    virtual void cleanuUp(void) {}
-
-    pthread_t getThreadId(void)
+    pthread_t getThreadID(void)
     {
         return m_tid;
     }
@@ -96,59 +182,20 @@ public:
         return rtn;
     }
 
-    static void mutexInit(pthread_mutex_t& mtx, mutextAttr_e mutexAttr = MUTEX_NORMAL_E);
+    /* 
+     *  thread body 
+     */
+    virtual void process(void) = 0;
 
-    static void mutexDestroy(pthread_mutex_t& mtx)
-    {
-        pthread_mutex_destroy(&mtx);
-    }
+    /*
+     *  initialize extra resources
+     */
+    virtual void onStartup(void) {}
 
-    static void condInit(pthread_cond_t& cond)
-    {
-        pthread_condattr_t condattr;
-
-        pthread_condattr_init(&condattr);
-        pthread_cond_init(&cond, &condattr);
-        pthread_condattr_destroy(&condattr);
-    }
-
-    static void lock(pthread_mutex_t& mtx)
-    {
-        pthread_mutex_lock(&mtx);
-    }
-
-    static void tryLock(pthread_mutex_t& mtx)
-    {
-        pthread_mutex_trylock(&mtx);
-    }
-
-    static void unlock(pthread_mutex_t& mtx)
-    {
-        pthread_mutex_unlock(&mtx);
-    }
-
-    static void condWait(pthread_cond_t& cond, pthread_mutex_t& mtx)
-    {
-        pthread_cond_wait(&cond, &mtx);
-    }
-
-    static void condTimedWait(pthread_cond_t& cond, pthread_mutex_t& mtx, const struct timespec& abstime)
-    {
-        pthread_cond_timedwait(&cond, &mtx, &abstime);
-    }
-
-    static void condSignal(pthread_cond_t& cond)
-    {
-        pthread_cond_signal(&cond);
-    }
-
-    static void condBroadcast(pthread_cond_t& cond)
-    {
-        pthread_cond_broadcast(&cond);
-    }
-
+    /*
+     *  release extra resources
+     */
+    virtual void onExit(void) {}
 };
-
-
 #endif
 
